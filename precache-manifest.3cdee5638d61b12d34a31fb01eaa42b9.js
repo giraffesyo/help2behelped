17219f172f1b56d7b85764b5a323f88f580298b5
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 16:37:26 -0500

    Updates

---
+++ precache-manifest.3cdee5638d61b12d34a31fb01eaa42b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "89e064f27bfc153e5cb8",
+    "url": "/static/js/main.89e064f2.chunk.js"
+  },
+  {
+    "revision": "6d79c7e786903f72a9bb",
+    "url": "/static/js/1.6d79c7e7.chunk.js"
+  },
+  {
+    "revision": "6d79c7e786903f72a9bb",
+    "url": "/static/css/1.3f54b75f.chunk.css"
+  },
+  {
+    "revision": "4b60bc0616b6370e79500e67f7b04ea8",
+    "url": "/index.html"
+  }
+];
