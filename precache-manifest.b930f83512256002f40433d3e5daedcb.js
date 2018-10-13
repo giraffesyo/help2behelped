4139a7f9496499b9857d3e39bf389e476f83c2a0
Michael McQuade <giraffesyo@gmail.com>
2018-10-13 16:51:57 -0500

    Updates

---
+++ precache-manifest.b930f83512256002f40433d3e5daedcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d6a341560c8a0e7d06b",
+    "url": "/help2behelped/static/js/runtime~main.6d6a3415.js"
+  },
+  {
+    "revision": "3c17063559842e847d9f",
+    "url": "/help2behelped/static/js/main.3c170635.chunk.js"
+  },
+  {
+    "revision": "27f5bd218d53b568a687",
+    "url": "/help2behelped/static/js/1.27f5bd21.chunk.js"
+  },
+  {
+    "revision": "27f5bd218d53b568a687",
+    "url": "/help2behelped/static/css/1.3f54b75f.chunk.css"
+  },
+  {
+    "revision": "307ba57b2fd85f2b6f0ce7cc49f75771",
+    "url": "/help2behelped/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3cdee5638d61b12d34a31fb01eaa42b9.js"
+  "/help2behelped/precache-manifest.b930f83512256002f40433d3e5daedcb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/help2behelped/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
